d651a9e6325094da389f6253fe20b183d39151f4
Aryann-04 <aryanzalavadia4@gmail.com>
2025-08-04 10:24:24 +0530

    inverter-management

---
+++ AZ0911/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzjvU0mCA6RQFaon3MKpI5XyifbFfIGTY",
+  authDomain: "az0911.firebaseapp.com",
+  projectId: "az0911",
+  storageBucket: "az0911.firebasestorage.app",
+  messagingSenderId: "981428933497",
+  appId: "1:981428933497:web:c5969cc1145db697d855a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
