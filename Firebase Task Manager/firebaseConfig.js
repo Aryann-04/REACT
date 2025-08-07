e05857c0672c62523aad22b2196859e4404006e6
Aryann-04 <aryanzalavadia4@gmail.com>
2025-08-07 11:55:09 +0530

    FIREBASE AUTH

--- Firebase Task Manager/firebaseConfig.js
+++ Firebase Task Manager/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const db = getFirestore(app)
-
-export {auth,db} 
+const provider = new GoogleAuthProvider;
+export {auth,db,provider} 
